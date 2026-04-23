bd2dd29af9231a135c89db30d2e3fa9cc356e41f
Dmitriy <digr98@gmail.com>
2026-04-23 21:08:25 +0300

    feat: types export & text color

--- src/components/Text/types.ts
+++ src/components/Text/types.ts
@@ -23,4 +23,5 @@ export type TextColor =
   | "tertiary"
   | "accent"
   | "danger"
-  | "white";
+  | "white"
+  | "success";
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import "./styles/index.scss";
 export * from "./components";
 export * from "./context";
+export * from "./types";
